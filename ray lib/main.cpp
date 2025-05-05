0bf3d99a475d0c9034042726033d224201c88501
fabian <fabianovich@duck.com>
2025-05-05 19:37:28 +0200

    window conf

--- ray lib/main.cpp
+++ ray lib/main.cpp
@@ -1,1 +1,24 @@
-#include <raylib.h>
+#include "raylib.h"
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "eat shit");
+
+    SetTargetFPS(60);              
+    while (!WindowShouldClose())    // window close button / esc key
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("poop!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    
+    CloseWindow();        
+    return 0;
+}

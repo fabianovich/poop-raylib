44fa037bb9bc3270bf7167f02cfde4bccb48cf98
fabian <fabianovich@duck.com>
2025-05-05 22:07:31 +0200

    poop text + font

--- ray lib/main.cpp
+++ ray lib/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+
 int main(void)
 {
     const int screenWidth = 800;
@@ -6,19 +7,28 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "eat shit");
 
-    SetTargetFPS(60);              
-    while (!WindowShouldClose())    // window close button / esc key
+    Font comicFont = LoadFont("resources/comic.ttf");
+
+    const char* message = "poop";
+    Vector2 position = { 200, 100 };
+    float fontSize = 200.0f;
+    float spacing = 2.0f;
+    Color tint = DARKGRAY;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
     {
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
 
-        DrawText("poop!", 190, 200, 20, LIGHTGRAY);
+        DrawTextEx(comicFont, message, position, fontSize, spacing, tint);
 
         EndDrawing();
     }
 
-    
-    CloseWindow();        
+    UnloadFont(comicFont);
+    CloseWindow();
+
     return 0;
 }

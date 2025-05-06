f67fc4259d0cda9f57dbfb5a0087b676c271174a
fabian <fabianovich@duck.com>
2025-05-06 10:34:50 +0200

    controls

--- ray lib/main.cpp
+++ ray lib/main.cpp
@@ -2,15 +2,21 @@
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+    void ToggleFullscreen(void);
 
     InitWindow(screenWidth, screenHeight, "eat shit");
 
     Font comicFont = LoadFont("resources/comic.ttf");
 
+
+    const int poopWidth = (screenWidth / 2) - 175;
+    const int poopHeight = (screenHeight / 2) - 125;
+
+
     const char* message = "poop";
-    Vector2 position = { 200, 100 };
+    Vector2 position = { poopWidth, poopHeight };
     float fontSize = 200.0f;
     float spacing = 2.0f;
     Color tint = DARKGRAY;
@@ -26,12 +32,32 @@ int main(void)
 
     SetTargetFPS(60);
 
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         BeginMode3D(camera);
+        
+        float offsetThisFrame = 20.0f * GetFrameTime();
+
+        if (IsKeyDown(KEY_RIGHT))
+        {
+            cubePosition.x += offsetThisFrame;
+        }
+        else if (IsKeyDown(KEY_LEFT))
+        {
+            cubePosition.x -= offsetThisFrame;
+        }
+        else if (IsKeyDown(KEY_UP))
+        {
+            cubePosition.z -= offsetThisFrame;
+        }
+        else if (IsKeyDown(KEY_DOWN))
+        {
+            cubePosition.z += offsetThisFrame;
+        }
 
         DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, BROWN);
         DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, BLACK);
